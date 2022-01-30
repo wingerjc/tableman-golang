c8f6206cb32aeb72983356c08e0000534f4c2cec
Jaymes Winger <rathmere@gmail.com>
2022-01-30 01:25:42 -0600

    Get preliminary file format complete, with tests.

--- pkg/parser/grammar.go
+++ pkg/parser/grammar.go
@@ -5,104 +5,147 @@ import (
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
-type TABLE_FILE struct {
-	Header *FileHeader `@@?`
-	Tables []*Table    `@@*`
+type TableFile struct {
+	Pos    lexer.Position
+	Header *FileHeader `parser:"EOL* @@"`
+	Tables []*Table    `parser:"(EOL+ @@? TableBarrier?)*"`
 }
 
 type FileHeader struct {
-	PackageName *string `@String`
+	Pos             lexer.Position
+	RootPackageName string             `parser:"PkgStart @TableName"`
+	SubPackages     []string           `parser:"(PkgDelimiter @TableName)*"`
+	Imports         []*ImportStatement `parser:"(EOL @@)*"`
+}
+
+type ImportStatement struct {
+	Pos      lexer.Position
+	FileName string "Import @FilePath"
 }
 
 type Table struct {
-	Header *TableHeader `@@`
-	Rows   []*TableRow  `@@+`
+	Pos    lexer.Position
+	Header *TableHeader `parser:"@@"`
+	Rows   []*TableRow  `parser:"(EOL @@)+"`
 }
 
 type TableHeader struct {
-	Name *string `@Ident`
+	Pos  lexer.Position
+	Name string `parser:"TableStart @TableName"`
+	Tags []*Tag `parser:"(EOL+ @@)*"`
+}
+
+type Tag struct {
+	Pos   lexer.Position
+	Key   RowLabel `parser:"TagStart @@ TableDelimiter"`
+	Value RowLabel `parser:"@@"`
 }
 
 type TableRow struct {
-	Default bool       `(@Default?`
-	Numbers *RangeList `@@?`
-	Label   *RowLabel  `@@? ":")?`
-	Values  []*RowItem `@@+`
+	Pos     lexer.Position
+	Default bool       `parser:"(@Default?"`
+	Weight  string     `parser:"@WeightMarker?"`
+	Numbers *RangeList `parser:"@@?"`
+	Label   *RowLabel  `parser:"@@? ':')?"`
+	Values  []*RowItem `parser:"@@+"`
 }
 
 type RowItem struct {
-	StringVal  *string     `@String`
-	Expression *Expression `| @@`
+	Pos        lexer.Position
+	StringVal  *string     `parser:"(@String"`
+	Expression *Expression `parser:"| @@)(ExtendLine EOL)?"`
 }
 
 type RowLabel struct {
-	Single  *string `@TableName`
-	Escaped *string `| @String`
+	Pos     lexer.Position
+	Single  *string `parser:"@TableName"`
+	Escaped *string `parser:"| @String"`
 }
 
 type RangeList struct {
-	First *NumberRange   `@@`
-	Rest  []*NumberRange `(","@@)*`
+	Pos   lexer.Position
+	First *NumberRange   `parser:"@@"`
+	Rest  []*NumberRange `parser:"(','@@)*"`
 }
 
 // NumberRange represents a single number ror a range.
 //
 type NumberRange struct {
-	First  *int `((@Number"-"`
-	Last   int  `@Number)`
-	Single int  `| @Number)`
+	Pos    lexer.Position
+	First  *int `parser:"((@Number'-'"`
+	Last   int  `parser:"@Number)"`
+	Single int  `parser:"| @Number)"`
 }
 
 type Expression struct {
-	Value string `"{" @Number "}"`
+	Pos   lexer.Position
+	Value string `parser:"'{' @Number '}'"`
 }
 
 type Roll struct {
+	Pos            lexer.Position
+	RollDice       string   `parser:"@Roll"`
+	RollSubset     string   `parser:"@RollSubset? ("`
+	RollFuncAggr   string   `parser:"@RollFuncAggr"`
+	RollCountAggrs []string `parser:"|(@RollCountAggr+))?"`
 }
 
 const (
-	NATURAL_NUMBER = `[1-9][0-9]*`
-	WHOLE_NUMBER   = `0|([1-9][0-9]*)`
-	INTEGER        = `0|(-?[1-9][0-9]*)`
+	NATURAL_NUMBER = `([1-9][0-9]*)`
+	WHOLE_NUMBER   = `(0|([1-9][0-9]*))`
+	INTEGER        = `(0|(-?[1-9][0-9]*))`
 )
 
 var (
 	fileLexer = lexer.MustStateful(lexer.Rules{
 		"Root": []lexer.Rule{
-			{"Default", `d(ef(ault)?)?`, nil},
+			{Name: "Default", Pattern: `d(ef(ault)?)?`},
+			{Name: "PkgStart", Pattern: `TablePack`},
+			{Name: "PkgDelimiter", Pattern: `\.`},
+			{Name: "WeightMarker", Pattern: `w=` + WHOLE_NUMBER},
+			{Name: "ExtendLine", Pattern: `->`},
+			{Name: "TableBarrier", Pattern: `--(-+)`},
+			{Name: "TableStart", Pattern: `[Tt](able)?:`},
+			{Name: "Import", Pattern: `(?i)import`},
+			{Name: "FilePath", Pattern: `(?i)f"(([A-Z]:)|~|(\.\.?))/.*"`},
 			lexer.Include("Atomic"),
-			{"Int", `[1-9][0-9]*`, nil},
-			{"TableCall", `[Tt]\(`, lexer.Push("TableCall")},
-			{"Expr", `{`, lexer.Push("Expr")},
-			{"EOL", `[\n\r]+`, nil},
-			{"ExtendLine", `->`, nil},
-			{"Comment", `#.*$`, nil},
-			{"Whitespace", `[ \t]+`, nil},
+			{Name: "ListDelimeter", Pattern: `,`},
+			{Name: "TableDelimiter", Pattern: `:`},
+			{Name: "RangeDash", Pattern: `-`},
+			{Name: "TableCall", Pattern: `[Tt]\(`, Action: lexer.Push("TableCall")},
+			{Name: "Expr", Pattern: `{`, Action: lexer.Push("Expr")},
+			{Name: "EOL", Pattern: `\r?\n`},
+			{Name: "Comment", Pattern: `#.*$`},
+			{Name: "CommentLine", Pattern: `^[ \t]*#.*\r?\n`},
+			{Name: "Whitespace", Pattern: `[ \t]+`},
+			{Name: "TagStart", Pattern: `~`},
 		},
 		"Atomic": []lexer.Rule{
-			{"TableName", `[a-zA-Z][a-zA-Z0-9_\-]*`, nil},
-			{"Roll", `[1-9][0-9]*d[1-9][0-9]*((l|h)[1-9][0-9]+)?(\.(min|max|sum|avg|mode))?`, nil},
-			{"String", `"(\\"|[^"])*"`, nil},
-			{"Number", `[1-9][0-9]*`, nil},
-			{"RangeDash", `-`, nil},
-			{"ListDelimeter", `,`, nil},
-			{"TableDelimeter", `:`, nil},
+			{Name: "TableName", Pattern: `[a-zA-Z][a-zA-Z0-9_\-]*`},
+			{Name: "Roll", Pattern: NATURAL_NUMBER + `d` + NATURAL_NUMBER, Action: lexer.Push("Roll")},
+			{Name: "String", Pattern: `"(\\"|[^"])*"`},
+			{Name: "Number", Pattern: NATURAL_NUMBER},
 		},
-
+		"Roll": []lexer.Rule{
+			{Name: "RollSubset", Pattern: `(l|h)` + NATURAL_NUMBER},
+			{Name: "RollFuncAggr", Pattern: `\.(min|max|sum|avg|mode)`},
+			{Name: "RollCountAggr", Pattern: `\.[+-]` + NATURAL_NUMBER + `(x` + NATURAL_NUMBER + `)?`},
+			{Name: "RollEnd", Pattern: `[ \t]+|$|(\r?\n)`, Action: lexer.Pop()},
+		},
 		"Expr": []lexer.Rule{
-			{"Number", NATURAL_NUMBER, nil},
-			{"ExprEnd", `}`, lexer.Pop()},
+			lexer.Include("Atomic"),
+			{Name: "ExprEnd", Pattern: `}`, Action: lexer.Pop()},
 		},
 		"TableCall": []lexer.Rule{
-			{"TableCallEnd", `\)`, lexer.Pop()},
+			{Name: "TableCallEnd", Pattern: `\)`, Action: lexer.Pop()},
 		},
 	})
 )
 
 func GetParser() (*participle.Parser, error) {
 	return participle.Build(
-		&TABLE_FILE{},
+		&TableFile{},
 		participle.Lexer(fileLexer),
-		participle.Elide("Comment", "Whitespace"),
+		participle.Elide("Comment", "Whitespace", "CommentLine"),
 	)
 }
--- pkg/parser/grammar_test.go
+++ pkg/parser/grammar_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func parserTypeWithDefaultOptions(t interface{}) (*participle.Parser, error) {
-	return participle.Build(t, participle.Lexer(fileLexer), participle.Elide("Comment", "Whitespace"))
+	return participle.Build(t, participle.Lexer(fileLexer), participle.Elide("Comment", "Whitespace", "CommentLine"))
 }
 
 func TestNumberRanges(t *testing.T) {
@@ -24,7 +24,7 @@ func TestNumberRanges(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(7, val.Single)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
 	val2 := &NumberRange{}
@@ -33,7 +33,7 @@ func TestNumberRanges(t *testing.T) {
 	assert.Equal(1, *val2.First)
 	assert.Equal(7, val2.Last)
 	if print {
-		pp.Print(val2)
+		pp.Println(val2)
 	}
 
 	p2, err := parserTypeWithDefaultOptions(&RangeList{})
@@ -44,7 +44,7 @@ func TestNumberRanges(t *testing.T) {
 	assert.NotNil(val3.First)
 	assert.Len(val3.Rest, 3)
 	if print {
-		pp.Print(val3)
+		pp.Println(val3)
 	}
 }
 
@@ -62,7 +62,7 @@ func TestRowLabel(t *testing.T) {
 	assert.NotNil(val.Escaped)
 	assert.Equal(expect, *val.Escaped)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
 	val = &RowLabel{}
@@ -72,25 +72,27 @@ func TestRowLabel(t *testing.T) {
 	assert.NotNil(val.Single)
 	assert.Equal(expect, *val.Single)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 }
 
 func TestTableRow(t *testing.T) {
-	print := true
+	print := false
 	t.Parallel()
 	assert := assert.New(t)
 	parser, err := parserTypeWithDefaultOptions(&TableRow{})
 	assert.NoError(err)
 
+	// Expression value
 	val := &TableRow{}
 	err = parser.ParseString("", `{4}`, val)
 	assert.NoError(err)
 	assert.Len(val.Values, 1)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Lbael and expression value.
 	val = &TableRow{}
 	expect := `"qux zed"`
 	err = parser.ParseString("", `zombie: {4} `+expect, val)
@@ -100,9 +102,10 @@ func TestTableRow(t *testing.T) {
 	assert.Len(val.Values, 2)
 	assert.Equal(expect, *val.Values[1].StringVal)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Range selector and string value
 	val = &TableRow{}
 	expect = `"grok"`
 	err = parser.ParseString("", `1-3,4,8: `+expect, val)
@@ -112,9 +115,10 @@ func TestTableRow(t *testing.T) {
 	assert.Len(val.Values, 1)
 	assert.Equal(expect, *val.Values[0].StringVal)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Two string values with numeeric and label selectors.
 	val = &TableRow{}
 	expects := []string{`"gleebrox"`, `"molduk"`}
 	err = parser.ParseString("", `6 zombie: `+expects[0]+` `+expects[1], val)
@@ -126,32 +130,255 @@ func TestTableRow(t *testing.T) {
 	assert.Equal(expects[0], *val.Values[0].StringVal)
 	assert.Equal(expects[1], *val.Values[1].StringVal)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Shortest default.
 	val = &TableRow{}
 	err = parser.ParseString("", `d: "zzz"`, val)
 	assert.NoError(err)
 	assert.True(val.Default)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Default as def and numric value.
 	val = &TableRow{}
 	err = parser.ParseString("", `def 6: "zzz"`, val)
 	assert.NoError(err)
 	assert.True(val.Default)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
+	// Default and label.
 	val = &TableRow{}
 	err = parser.ParseString("", `default axes: "xyz"`, val)
 	assert.NoError(err)
 	assert.True(val.Default)
 	if print {
-		pp.Print(val)
+		pp.Println(val)
 	}
 
-	assert.Fail("foo")
+	// Weighted rows
+	val = &TableRow{}
+	err = parser.ParseString("", `w=36: "xyz"`, val)
+	assert.NoError(err)
+	assert.Equal("w=36", val.Weight)
+	if print {
+		pp.Println(val)
+	}
+
+	// Multiple line lists.
+	val = &TableRow{}
+	err = parser.ParseString("", "\"xyz\" ->\n    \"asdf\" \"qwer\" ->\r\n \t\"ffff\"", val)
+	assert.NoError(err)
+	assert.Len(val.Values, 4)
+	if print {
+		pp.Println(val)
+	}
+}
+
+func TestTableHeader(t *testing.T) {
+	print := false
+	t.Parallel()
+	assert := assert.New(t)
+	parser, err := parserTypeWithDefaultOptions(&TableHeader{})
+	assert.NoError(err)
+
+	val := &TableHeader{}
+	err = parser.ParseString("", `t: table1`, val)
+	assert.NoError(err)
+	assert.Equal("table1", val.Name)
+	assert.Len(val.Tags, 0)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &TableHeader{}
+	err = parser.ParseString("", "T: table1 \n ~ author: franz", val)
+	assert.NoError(err)
+	assert.Equal("table1", val.Name)
+	assert.Len(val.Tags, 1)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &TableHeader{}
+	err = parser.ParseString("", "T: table1 \n ~ author: franz \n ~ \"create date\": \"10/01/2001\"", val)
+	assert.NoError(err)
+	assert.Equal("table1", val.Name)
+	assert.Len(val.Tags, 2)
+	if print {
+		pp.Println(val)
+	}
+}
+
+func TestTable(t *testing.T) {
+	print := false
+	t.Parallel()
+	assert := assert.New(t)
+	parser, err := parserTypeWithDefaultOptions(&Table{})
+	assert.NoError(err)
+
+	val := &Table{}
+	table := `t: footable
+	~ license: free
+	1: "asdf"
+	"green"
+	w=3: {4}
+	foo: "once"
+	d: "red"`
+	err = parser.ParseString("", table, val)
+	assert.NoError(err)
+	assert.Equal("footable", val.Header.Name)
+	assert.Len(val.Header.Tags, 1)
+	assert.Len(val.Rows, 5)
+	assert.True(val.Rows[4].Default)
+	if print {
+		pp.Println(val)
+	}
+}
+
+func TestFileHeader(t *testing.T) {
+	print := false
+	t.Parallel()
+	assert := assert.New(t)
+	parser, err := parserTypeWithDefaultOptions(&FileHeader{})
+	assert.NoError(err)
+
+	val := &FileHeader{}
+	err = parser.ParseString("", `TablePack Main`, val)
+	assert.NoError(err)
+	assert.Equal("Main", val.RootPackageName)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &FileHeader{}
+	err = parser.ParseString("", `TablePack Foo.Bar.Baz.quz.z3d`, val)
+	assert.NoError(err)
+	assert.Equal("Foo", val.RootPackageName)
+	assert.Len(val.SubPackages, 4)
+	assert.Equal("z3d", val.SubPackages[3])
+	if print {
+		pp.Println(val)
+	}
+
+	val = &FileHeader{}
+	header := `TablePack garbo
+	import f"~/tables/1.tab"
+	IMPORT f"c:/blah/blerf"`
+	err = parser.ParseString("", header, val)
+	assert.NoError(err)
+	assert.Equal("garbo", val.RootPackageName)
+	assert.Len(val.SubPackages, 0)
+	assert.Len(val.Imports, 2)
+	if print {
+		pp.Println(val)
+	}
+}
+
+func TestTableFile(t *testing.T) {
+	print := false
+	t.Parallel()
+	assert := assert.New(t)
+	parser, err := parserTypeWithDefaultOptions(&TableFile{})
+	assert.NoError(err)
+
+	val := &TableFile{}
+	err = parser.ParseString("", `TablePack Main`, val)
+	assert.NoError(err)
+	assert.Equal("Main", val.Header.RootPackageName)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &TableFile{}
+	file := `TablePack Main
+
+	t: colors
+	"red"
+	"green"
+	# some note
+	"blue"
+	---
+
+
+	t: numbers
+	~ size: "7"
+	~ "weight max": forty-eight
+	a: "One"
+	5: "two"
+	d: {3}
+
+
+	`
+	err = parser.ParseString("", file, val)
+	assert.NoError(err)
+	assert.Equal("Main", val.Header.RootPackageName)
+	assert.Len(val.Tables, 2)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &TableFile{}
+	file = `TablePack quacko
+	import f"~/jeremy"
+
+	Table: colors
+	d w=4 1-5,8 red: "red"
+	"blue"
+
+	#----------
+	table: numbers
+	~ size: "7"
+	~ "weight max": forty-eight
+	a: "One"
+	5: "two"
+	d: {3}`
+	err = parser.ParseString("", file, val)
+	assert.NoError(err)
+	assert.Equal("quacko", val.Header.RootPackageName)
+	assert.Len(val.Tables, 2)
+	assert.Len(val.Tables[0].Rows, 2)
+	if print {
+		pp.Println(val)
+	}
+}
+
+func TestRoll(t *testing.T) {
+	print := false
+	t.Parallel()
+	assert := assert.New(t)
+	parser, err := parserTypeWithDefaultOptions(&Roll{})
+	assert.NoError(err)
+
+	val := &Roll{}
+	err = parser.ParseString("", `3d8`, val)
+	assert.NoError(err)
+	assert.Equal("3d8", val.RollDice)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &Roll{}
+	err = parser.ParseString("", `10d20h5.mode`, val)
+	assert.NoError(err)
+	assert.Equal("10d20", val.RollDice)
+	assert.Equal("h5", val.RollSubset)
+	assert.Equal(".mode", val.RollFuncAggr)
+	if print {
+		pp.Println(val)
+	}
+
+	val = &Roll{}
+	err = parser.ParseString("", `10d20.+1x2.-20x1`, val)
+	assert.NoError(err)
+	assert.Equal("10d20", val.RollDice)
+	assert.Len(val.RollCountAggrs, 2)
+	assert.Equal(".+1x2", val.RollCountAggrs[0])
+	if print {
+		pp.Println(val)
+	}
 }
